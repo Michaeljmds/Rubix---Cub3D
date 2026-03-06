f4573a89db7b2b89dcd8e6049ea9d7b2756f2cc1
Shirley Hidrobo Cordova <shhidrob@student.42london.com>
2026-03-06 23:06:51 +0000

    fix Libft

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mimacdou <mimacdou@student.42.fr>          +#+  +:+       +#+        */
+/*   By: shhidrob <shhidrob@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 17:01:23 by mimacdou          #+#    #+#             */
-/*   Updated: 2025/01/13 18:42:29 by mimacdou         ###   ########.fr       */
+/*   Updated: 2026/03/06 23:04:15 by shhidrob         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,11 @@
 void	*ft_calloc(size_t num, size_t size)
 {
 	void	*array;
-	size_t	i;
 
 	if (num == 0 || size == 0)
 		return (malloc(0));
 	if (num && SIZE_MAX / num < size)
 		return (NULL);
-	i = 0;
 	array = malloc(num * size);
 	if (!array)
 		return (NULL);
